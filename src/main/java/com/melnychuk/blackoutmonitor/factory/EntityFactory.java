281f82bf34bf84e89aaf7290d2a0da502bee6c84
Yehor Melnychuk <myegor15@gmail.com>
2023-02-20 16:45:49 +0200

    EntityFactory small refactor

--- src/main/java/com/melnychuk/blackoutmonitor/factory/EntityFactory.java
+++ src/main/java/com/melnychuk/blackoutmonitor/factory/EntityFactory.java
@@ -2,7 +2,7 @@ package com.melnychuk.blackoutmonitor.factory;
 
 import com.melnychuk.util.jdbc.rs.RSRowMapper;
 
-import java.util.Collection;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -10,19 +10,22 @@ import java.util.stream.Collectors;
 public interface EntityFactory<VO, DTO> extends RSRowMapper<VO> {
 
     VO createVO(DTO dto);
-
     DTO createDTO(VO vo);
 
     default List<DTO> createDTOList(List<VO> voList) {
-        return Optional.ofNullable(voList).stream()
-                .flatMap(Collection::stream)
+        return Optional
+                .ofNullable(voList)
+                .orElseGet(ArrayList::new)
+                .stream()
                 .map(this::createDTO)
                 .collect(Collectors.toList());
     }
 
     default List<VO> createVOList(List<DTO> dtoList) {
-        return Optional.ofNullable(dtoList).stream()
-                .flatMap(Collection::stream)
+        return Optional
+                .ofNullable(dtoList)
+                .orElseGet(ArrayList::new)
+                .stream()
                 .map(this::createVO)
                 .collect(Collectors.toList());
     }
