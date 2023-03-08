7cec3fdeb86e1c7cf8d7a44d731f1f05b7c10fe9
Yehor Melnychuk <myegor15@gmail.com>
2023-03-08 20:30:30 +0200

    return empty collections for mappers

--- src/main/java/com/melnychuk/blackoutmonitor/mapper/EntityMapper.java
+++ src/main/java/com/melnychuk/blackoutmonitor/mapper/EntityMapper.java
@@ -1,9 +1,12 @@
 package com.melnychuk.blackoutmonitor.mapper;
 
 import com.melnychuk.util.jdbc.rs.RSMapper;
+import org.mapstruct.IterableMapping;
 import org.mapstruct.MappingTarget;
 
 import java.util.List;
+
+import static org.mapstruct.NullValueMappingStrategy.RETURN_DEFAULT;
 
 public interface EntityMapper<M, DTO> extends RSMapper<M> {
 
@@ -12,7 +15,10 @@ public interface EntityMapper<M, DTO> extends RSMapper<M> {
 
     void updateDTO(M model, @MappingTarget DTO dto);
 
+    @IterableMapping(nullValueMappingStrategy = RETURN_DEFAULT)
     List<M> toModelList(List<DTO> dtoList);
+
+    @IterableMapping(nullValueMappingStrategy = RETURN_DEFAULT)
     List<DTO> toDTOList(List<M> modelList);
 
 }
